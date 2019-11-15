68bed6dc54579be8159a0c2c88ad8763fa5fb3af
m.slavoshevski@cian.ru <m.slavoshevski@cian.ru>
2019-11-15 09:21:54 +0300

    find_packages()

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sqlep',
     version='0.0.1',
-    packages=['sqlep'],
+    packages=find_packages(),
     url='github.com/cian-github/sqlep',
     license='Apache 2.0',
     author='cian dream team',
